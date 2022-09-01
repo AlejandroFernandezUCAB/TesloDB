e5bbcbe6fe61a76599bcf4436c0007cf294a09c5
pfernandez-payall <pfernandez@payall.com.ve>
2022-09-01 15:47:33 -0400

    Clase 4

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -68,8 +68,28 @@ export class ProductsService {
 		return product;
 	}
 
-	update(id: number, updateProductDto: UpdateProductDto) {
-		return `This action updates a #${id} product`;
+	async update(id: string, updateProductDto: UpdateProductDto) {
+		const productValidation = await this.findOne(updateProductDto.slug);
+
+		if (updateProductDto.slug === productValidation.slug)
+			throw new BadRequestException(
+				`Value of ${updateProductDto.slug} is duplicated`,
+			);
+
+		const product = await this.productRepository.preload({
+			id: id,
+			...updateProductDto,
+		});
+
+		if (!product)
+			throw new NotFoundException(`Product with id: ${id} not found`);
+
+		try {
+			await this.productRepository.save(product);
+			return product;
+		} catch (error) {
+			this.handleDBExceptions(error);
+		}
 	}
 
 	async remove(id: string) {
