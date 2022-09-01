5bd67be965e64665201713e9c75f8835020865d9
pfernandez-payall <pfernandez@payall.com.ve>
2022-09-01 15:13:11 -0400

    Clase 3

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,11 +6,13 @@ import {
 	Patch,
 	Param,
 	Delete,
+	Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ParseUUIDPipe } from '@nestjs/common';
+import { PaginationDto } from '../common/dtos/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -22,12 +24,12 @@ export class ProductsController {
 	}
 
 	@Get()
-	findAll() {
-		return this.productsService.findAll();
+	findAll(@Query() paginationDto: PaginationDto) {
+		return this.productsService.findAll(paginationDto);
 	}
 
-	@Get(':id')
-	findOne(@Param('id', ParseUUIDPipe) id: string) {
+	@Get(':term')
+	findOne(@Param('term') id: string) {
 		return this.productsService.findOne(id);
 	}
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,7 +10,8 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
-
+import { PaginationDto } from '../common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 @Injectable()
 export class ProductsService {
 	private readonly logger = new Logger('ProductsService');
@@ -31,17 +32,38 @@ export class ProductsService {
 	}
 
 	//TODO: Paginar
-	findAll() {
-		return this.productRepository.find({});
+	findAll(paginationDto: PaginationDto) {
+		const { limit = 10, offset = 0 } = paginationDto;
+		return this.productRepository.find({
+			take: limit,
+			skip: offset,
+			//TODO: relaciones
+		});
 	}
 
-	async findOne(id: string) {
-		const product = await this.productRepository.findOneBy({
-			id,
-		});
+	async findOne(term: string) {
+		let product: Product;
+
+		if (isUUID(term)) {
+			product = await this.productRepository.findOneBy({
+				id: term,
+			});
+		} else {
+			const queryBuilder = this.productRepository.createQueryBuilder();
+			product = await queryBuilder
+				.where('UPPER(title) =:title or slug =:slug', {
+					title: term.toUpperCase(),
+					slug: term.toLocaleLowerCase(),
+				})
+				.getOne();
+		}
+
+		// const product = await this.productRepository.findOneBy({
+		// 	id,
+		// });
 
 		if (!product)
-			throw new NotFoundException(`Product with id ${id} not found`);
+			throw new NotFoundException(`Product with ${term} not found`);
 
 		return product;
 	}
