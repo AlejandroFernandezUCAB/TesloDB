2480c33e132755b20abfb9fbea3fbb9e9544c6b0
pfernandez-payall <pfernandez@payall.com.ve>
2022-09-19 11:40:09 -0400

    clase 8

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -12,6 +12,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
+import { ProductImage } from './entities/product-image.entity';
 @Injectable()
 export class ProductsService {
 	private readonly logger = new Logger('ProductsService');
@@ -19,13 +20,23 @@ export class ProductsService {
 	constructor(
 		@InjectRepository(Product)
 		private readonly productRepository: Repository<Product>,
+
+		@InjectRepository(ProductImage)
+		private readonly productImageRepository: Repository<ProductImage>,
 	) {}
 
 	async create(createProductDto: CreateProductDto) {
 		try {
-			const product = this.productRepository.create(createProductDto);
+			const { images = [], ...productDetails } = createProductDto;
+
+			const product = this.productRepository.create({
+				...productDetails,
+				images: images.map((image) =>
+					this.productImageRepository.create({ url: image }),
+				),
+			});
 			await this.productRepository.save(product);
-			return product;
+			return { ...product, images: images };
 		} catch (error) {
 			this.handleDBExceptions(error);
 		}
@@ -72,6 +83,7 @@ export class ProductsService {
 		const product = await this.productRepository.preload({
 			id: id,
 			...updateProductDto,
+			images: [],
 		});
 
 		console.log(product);
